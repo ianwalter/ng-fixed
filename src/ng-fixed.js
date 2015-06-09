c06e2d988ab53a2b084541323a4b11ce61b51dd2
Ian Walter <ianwalter@fastmail.com>
2015-06-09 17:05:19 -0400

    Switching to fixed-element-selector and other changes

--- src/ng-fixed.js
+++ src/ng-fixed.js
@@ -1,5 +1,5 @@
 /**
- * ng-fixed - v0.0.1 - An AngularJS directive that changes an element's position
+ * ng-fixed - v0.2.0 - An AngularJS directive that changes an element's position
  * to fixed when scrolled to a given point
  *
  * @author Ian Kennington Walter <ianwatler@fastmail.com>
@@ -14,19 +14,60 @@
         restrict: 'A',
         scope: {
           'ngFixed': '&ngFixed',
-          'fixedPoint': '@fixedPoint',
-          'fixedClass': '@fixedClass'
+          'fixedElementSelector': '@fixedElementSelector',
+          'fixedClass': '@fixedClass',
+          'unfixedElementSelector': '@unfixedElementSelector'
         },
         link: ($scope, element) => {
           var added = false;
           var updateElement = () => {
-            var yOffset = (window.pageYOffset || document.scrollTop) -
-              (document.clientTop || 0);
-            if ($scope.ngFixed !== false && yOffset >= $scope.fixedPoint) {
-              element.addClass($scope.fixedClass);
-              added = true;
+            var pageYOffset = window.pageYOffset || document.scrollTop,
+                clientTop = document.clientTop || 0,
+                elementRect = element[0].getBoundingClientRect(),
+                fixedElement, fixedElementYOffset,
+                unfixedElement, unfixedElementYOffset, elementBottomYOffset;
+            var yOffset = pageYOffset - clientTop;
+
+            fixedElement = document.querySelector($scope.fixedElementSelector);
+
+            if (fixedElement) {
+              fixedElementYOffset = fixedElement.getBoundingClientRect().top +
+                pageYOffset;
+            }
+
+            if ($scope.ngFixed !== false &&
+                fixedElementYOffset &&
+                yOffset >= fixedElementYOffset) {
+
+              if ($scope.unfixedElementSelector) {
+                unfixedElement = document.querySelector(
+                  $scope.unfixedElementSelector
+                );
+                if (unfixedElement) {
+                  unfixedElementYOffset = unfixedElement
+                    .getBoundingClientRect().top + pageYOffset;
+                  elementBottomYOffset = elementRect.height + pageYOffset;
+                }
+              }
+
+              if (unfixedElementYOffset &&
+                  unfixedElementYOffset <= elementBottomYOffset) {
+                element.css(
+                  'top',
+                  unfixedElementYOffset - elementRect.height + 'px'
+                );
+                element.removeClass($scope.fixedClass);
+                added = false;
+              } else if (!added &&
+                         (!unfixedElementYOffset ||
+                          unfixedElementYOffset >= elementBottomYOffset)) {
+                element.css('top', '');
+                element.addClass($scope.fixedClass);
+                added = true;
+              }
             } else if (added) {
               element.removeClass($scope.fixedClass);
+              added = false;
             }
           };
 
